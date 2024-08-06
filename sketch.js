47e1f11885091e2e7e1a83768939e1433e788987
LUIZGAT <luizfelipeleitedomingues36@gmail.com>
2024-08-06 08:56:53 -0300

    substituindo codigos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
-html, body {
-    margin: 0;
-    padding: 0;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
+function setup() {
+  createCanvas(400, 400);
+   background(color(100,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+
+function draw() {
+  
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,50);
+ 
+  
+  
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
   }
-  canvas {
-    display: block;
+  
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
   }
+  
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+    
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+  
+  if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+    
+}
